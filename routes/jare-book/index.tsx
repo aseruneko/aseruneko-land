6350817a9970797c0524a64430d5d094ff5fad1f
aseruneko <aseruneko@gmail.com>
2023-10-07 22:06:19 +0900

    jare-book v2.0.5

--- routes/jare-book/index.tsx
+++ routes/jare-book/index.tsx
@@ -29,7 +29,7 @@ export default function JareBook() {
           >
             <h3>更新履歴</h3>
             <dl>
-              <dt>v2.0.4</dt>
+              <dt>v2.0.5</dt>
               <dd>@2023-10-07 更新検知の仕組みを改善</dd>
               <dt>v2.0.4</dt>
               <dd>@2023-10-07 ランダム単語機能</dd>
