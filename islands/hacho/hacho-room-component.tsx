0c4fcaa985556caa9ba88285fe0db6d9063ee74a
aseruneko <aseruneko@gmail.com>
2023-10-14 17:39:51 +0900

    hacho v1.0.0

--- islands/hacho/hacho-room-component.tsx
+++ islands/hacho/hacho-room-component.tsx
@@ -49,7 +49,7 @@ export default function HachoRoomComponent(props: HachoRoomComponentProps) {
     userName.value = window.localStorage.getItem("hacho-userName") ?? undefined;
     password.value = window.localStorage.getItem("hacho-password") ?? undefined;
   }
-  self.addEventListener("DOMContentLoaded", onLoad);
+  self.addEventListener("load", onLoad);
   return (
     <>
       {(hacho.value && !isMember.value)
